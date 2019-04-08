f931a64c4c437962fcb6351233bc1ac230ed325d
Martinq76 <martin.queguiner.42@gmail.com>
2019-04-08 12:09:44 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeColor1() {
+  document.getElementById('text').style.color = 'green';
+}
+
+function changeColor2() {
+  document.getElementById('text').style.color = 'red';
+}
+
+function changeColor3() {
+  document.getElementById('text').style.color = 'blue';
+}
